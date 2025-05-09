a1fb3fe384195b86a372d93169ced9a634979c71
Certified Raceist <146831806+CertifiedRaceist@users.noreply.github.com>
2025-05-09 13:50:28 +0200

    Add files via upload

---
+++ egzamin 5/skrypt.js
@@ -0,0 +1,16 @@
+function funkcja(){
+    let price = 0;
+    if (document.getElementById("peeling").checked){
+        price += 45;
+    }
+    if (document.getElementById("maska").checked){
+        price += 30;
+    }
+    if (document.getElementById("masaz").checked){
+        price += 20;
+    }
+    if (document.getElementById("makijaz").checked){
+        price += 50;
+    }
+    document.getElementById("p").innerText = "Cena zabiegów: "+ price;
+}
